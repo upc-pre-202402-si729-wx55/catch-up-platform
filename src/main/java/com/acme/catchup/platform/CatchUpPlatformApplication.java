2af97fbf97439cf6faa9e39ea9a1d0e14678f78d
Angel Velasquez <pcisavel@upc.edu.pe>
2024-10-14 20:34:26 -0500

    chore: setup project.

---
+++ src/main/java/com/acme/catchup/platform/CatchUpPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.acme.catchup.platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatchUpPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CatchUpPlatformApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/acme/catchup/platform/CatchUpPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.acme.catchup.platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatchUpPlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
